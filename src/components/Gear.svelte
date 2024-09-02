042be7819538ac792f4406dc78510d8ff2875ad8
Lunarmagpie <bambolambo0@gmail.com>
2024-09-01 21:05:58 -0400

    tweaks

--- src/components/Gear.svelte
+++ src/components/Gear.svelte
@@ -1,6 +1,4 @@
 <script>
-  console.log(document);
-
   $: rotation = 0;
 
   function getRotation() {
@@ -17,14 +15,7 @@
     transform: scale(25%) translate(-45%, 45%);
     transform-origin: bottom left;
     filter: brightness(0) invert(100%);
-    visibility: visible;
 	}
-
-  @media (orientation: portrait) {
-  	.gear {
-      visibility: hidden;
-    }
-  }
 </style>
 
 <div class="gear">
