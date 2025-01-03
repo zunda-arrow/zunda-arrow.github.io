31873cc42ec5c5a1fa95a68756d15bea6b78cb38
Lunarmagpie <Bambolambo0@gmail.com>
2025-01-02 22:05:15 -0500

    Improve buttons

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -21,18 +21,38 @@
    		margin: 0px;
     }
 
+    .button-selected {
+   		background-color: #cccccc;
+   		border: 1px;
+   		border-color: #ffffff;
+   		color: #000000;
+   		border-style: solid;
+   		padding: 5px;
+   		margin: 0px;
+    }
+
     .no-margin {
         margin: 0px;
     }
 </style>
 
 <div>
-    <button on:click={selectIncorrect} class="button">
-        Incorrect
-    </button>
-    <button on:click={selectCorrect} class="button">
-        Correct
-    </button>
+    {#if selected == "Correct"}
+        <button on:click={selectIncorrect} class="button">
+            Incorrect
+        </button>
+        <button on:click={selectCorrect} class="button-selected">
+            Correct
+        </button>
+    {/if}
+    {#if selected == "Incorrect"}
+        <button on:click={selectIncorrect} class="button-selected">
+            Incorrect
+        </button>
+        <button on:click={selectCorrect} class="button">
+            Correct
+        </button>
+    {/if}
     <div>
         {#if selected == "Correct"}
             <slot name="Correct" class="no-margin"/>
